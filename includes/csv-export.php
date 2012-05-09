428106e93a91b802ae16ef790b0c5a5570735847
bielefeldt <bielefeldt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-09 16:44:00 +0000

    edit export file
    git-svn-id: https://plugins.svn.wordpress.org/response-promotion-redeemer/tags/1.1@541919 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/csv-export.php
+++ includes/csv-export.php
@@ -4,13 +4,12 @@
 	******************************/
 	define('RM_PLUGIN_SLUG', 'response-promo-redeemer');
 	define('RM_PLUGIN_DIR', WP_PLUGIN_URL.'/'.RM_PLUGIN_SLUG.'/');	
-	wp_nonce_field(plugin_basename(__FILE__), 'wp_promo_noncename');
     $dirNAME = plugin_dir_url( __FILE__ );
 	define('RM_SITE', 'thepowertoprovoke.com');
 	
 	if(!function_exists('fn_rm_csv_export')){
 		function fn_rm_csv_export(){        
-			include_once($dirNAME.'/includes/rm-csv-generate.php');
+			include_once(RM_PLUGIN_DIR.'/includes/rm-csv-generate.php');
 			$req_table = isset($_REQUEST['rmcsv']) ? $_REQUEST['rmcsv'] : '';
 			if ($req_table){          
 				echo fn_rm_csv_gen($req_table);
