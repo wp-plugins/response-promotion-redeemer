95d6a720a16c63777e667e5c6b42062331caa176
bielefeldt <bielefeldt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-08 17:19:20 +0000

    added shortcode attr
    git-svn-id: https://plugins.svn.wordpress.org/response-promotion-redeemer/tags/1.0.6@554923 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/form-shortcodes.php
@@ -0,0 +1,69 @@
+<?php
+
+/******************************
+* add shortcodes
+******************************/
+
+
+
+function promo_form( $atts, $content = null ) {
+	$wp_wall_plugin_url = trailingslashit( get_bloginfo('wpurl') ).PLUGINDIR.'/'. dirname( plugin_basename(__FILE__) );
+	global $wpdb;
+	global $post;
+	global $rpr_db_version;
+	global $table_name;
+	$table_name .= '_'.$post->ID;
+	$from = get_post_meta( $post->ID, 'from', true );
+	$urllink = get_post_meta( $post->ID, 'urllink', true );
+	$ptype = get_post_meta( $post->ID, 'ptype', true );
+	$from = get_post_meta( $post->ID, 'from', true );
+	$redURL = get_post_meta( $post->ID, 'redURL', true );
+	if ($ptype == 'Connect') {
+		$conURL = get_post_meta( $post->ID, 'conURL', true );
+		$conNAME = get_post_meta( $post->ID, 'conNAME', true );
+		$conTNAME = get_post_meta( $post->ID, 'conTNAME', true );
+		$conUSER = get_post_meta( $post->ID, 'conUSER', true );
+		$conPASSWORD = get_post_meta( $post->ID, 'conPASSWORD', true );
+		$conQSTRING = get_post_meta( $post->ID, 'conQSTRING', true );
+	} else if ($ptype == 'Query') {
+		$queV1 = get_post_meta( $post->ID, 'queV1', true );
+		$queV2 = get_post_meta( $post->ID, 'queV2', true );
+		$queV3 = get_post_meta( $post->ID, 'queV3', true );
+	}
+	
+   extract( shortcode_atts( array(
+      'end_message' => '',
+      'attr_2' => 'attribute 2 default',
+      // ...etc
+      ), $atts ) );
+	  $form = '<div class="message" style="display:none; "><div id="alert" style="padding:10px; border:1px solid #999; background-color:#E7E7E7;"></div></div>';
+	  $form .= '<form id="contactform" class="appnitro"  method="post" action="'.$wp_wall_plugin_url.'/promo-codes.php"><ul>';
+	  $form .= '<li id="li_1" ><label class="description" for="name"><span class="listTitle">Name: </span></label><div><input id="name" name="name" class="element text medium fullwidth" type="text" maxlength="255" value=""/> </div> </li>';
+	  $form .= '<li id="li_10_0" ><label class="description" for="email"><span class="listTitle">Email: </span></label><div><input id="email" name="email" class="element text medium fullwidth" type="text" maxlength="255" value=""/> </div> </li>';
+	  $form .= '<li id="li_2" ><label class="description" for="license"><span class="listTitle">Redemption Code: </span></label><div><input id="license" name="license" class="element text medium fullwidth" type="password" maxlength="255" value=""/> </div> </li>';
+	  $form .= '<span style="display:none;"><p>Honeypot:</p> <input type="text" name="last" value="" id="last" /></span>';
+	  $form .= '
+	  	<input type="hidden" name="tablename" value="'.$table_name.'" id="" />
+		<input type="hidden" name="from_email" value="'.$from.'" id="" />
+		<input type="hidden" name="ptype" value="'.$ptype.'" id="" />
+		<input type="hidden" name="redURL" value="'.$redURL.'" id="" />
+		<input type="hidden" name="end_message" value="'.esc_attr($end_message).'" id="" />';
+	  if ($ptype == 'Connect') {
+		$form .= '<input type="hidden" name="conURL" value="'.$conURL.'" id="" />
+		<input type="hidden" name="conNAME" value="'.$conNAME.'" id="" />
+		<input type="hidden" name="conTNAME" value="'.$conTNAME.'" id="" />
+		<input type="hidden" name="conUSER" value="'.$conUSER.'" id="" />
+		<input type="hidden" name="conPASSWORD" value="'.$conPASSWORD.'" id="" />
+		<input type="hidden" name="conQSTRING" value="'.$conQSTRING.'" id="" />
+		<input type="hidden" name="queURL" value="'.$queURL.'" id="" />
+		<input type="hidden" name="end_message" value="'.esc_attr($end_message).'" id="" />';
+	  } else if ($ptype == 'Query') {
+		$form .= '<input type="hidden" name="queV1" value="'.$queV1.'" id="" /><input type="hidden" name="queV2" value="'.$queV2.'" id="" /><input type="hidden" name="queV3" value="'.$queV3.'" id="" />';
+	  }
+		$form .= '<input type="hidden" name="pID" value="'.$post->ID.'" id="" />
+	  ';
+	  $form .= '<li class="buttons"><input type="submit" name="submit" class="sendit promo_submit" value="SUBMIT"> </li>';
+	  $form .= '</ul></form><script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script><script type="text/javascript" src="'.$wp_wall_plugin_url.'/js/jquery.form.js"></script><script type="text/javascript" src="'.$wp_wall_plugin_url.'/js/jquery.formfooter.js"></script>';
+	  return $form;
+}
+add_shortcode( 'promo-form', 'promo_form' );
---
+++ includes/promo-codes.php
@@ -0,0 +1,186 @@
+<?php
+define( 'BLOCK_LOAD', true );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-config.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-includes/wp-db.php' );
+global $wpdb; 
+
+$postID = $_REQUEST['pID'];
+
+// parse query string
+
+
+$the_referer = $_SERVER["HTTP_REFERER"];
+//        Who you want to recieve the emails from the form. (Hint: generally you.)
+$sendto = $_REQUEST['email'];
+$bcc = 'bbielefeldt@thepowertoprovoke.com';
+$sendFrom = $_REQUEST['from_email'];
+$redURL = $_REQUEST['redURL'];
+$table_name = $_REQUEST['tablename'] ;
+$ptype = $_REQUEST['ptype'] ;
+$end_message = $_REQUEST['end_message'] ;
+
+
+//        The subject you'll see in your inbox
+$subject = 'Promotion portal submission at '.$the_referer;
+
+//        Message for the user when he/she doesn't fill in the form correctly.
+$errormessage = 'Oops! There seems to have been a problem. May we suggest...';
+
+//        Message for the user when he/she fills in the form correctly.
+$thanks = "Thank you for your submission your Partner promotion code will be emailed to you go check...";
+
+//        Message for the bot when it fills in in at all.
+$honeypot = "You filled in the honeypot! If you're human, try again!";
+
+//        Various messages displayed when the fields are empty.
+$emptyname =  'Entering your Name?';
+
+$emptylicense =  'Entering your Promotion code?';
+
+$emptyemail = 'Entering your Email Address?';
+
+
+//       Various messages displayed when the fields are incorrectly formatted.
+$alertname =  'Entering your Name using only the standard alphabet?';
+
+$alertlicense =  'Entering your Promotion Code using only the standard alphanumeric characters?';
+$invalidlicense =  'The Promotion Code you entered is invalid, please try again';
+
+$alertemail = 'Entering your Email in this format: <i>name@example.com</i>?';
+
+// --------------------------- Thats it! don't mess with below unless you are really smart! ---------------------------------
+
+//Setting used variables.
+$alert = '';
+$pass = 0;
+
+$license = $_REQUEST['license'];
+$license = str_replace("*%", "1", $license, $count);
+$license = str_replace("/&*o", "5", $license, $count);	
+$data_count = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM $table_name WHERE our_code = '".$license."'" ) );
+$fivesdrafts = $wpdb->get_results( $wpdb->prepare( "SELECT time_entered, partner_code, user_email, user_name FROM $table_name WHERE our_code = '".$license."'" ) );
+foreach ( $fivesdrafts as $fivesdraft ) {
+			$time = $fivesdraft->time_entered;
+			$partner_code = $fivesdraft->partner_code;
+			$partner_code = str_replace("*%", "6", $partner_code, $count);
+			$partner_code = str_replace("/&*o", "2", $partner_code, $count);
+			$user_email = $fivesdraft->user_email;
+			$user_name = $fivesdraft->user_name;
+		}
+
+// Sanitizing the data, kind of done via error messages first. Twice is better!
+function clean_var($variable) {
+    $variable = strip_tags(stripslashes(trim(rtrim($variable))));
+  return $variable;
+}
+
+//The first if for honeypot.
+if ( empty($_REQUEST['last']) ) {
+
+	// A bunch of if's for all the fields and the error messages.
+	if ( empty($_REQUEST['name']) ) {
+		$pass = 1;
+		$alert .= "<li>" . $emptyname . "</li>";
+	} elseif ( ereg( "[][{}()*+?.\\^$|]", $_REQUEST['name'] ) ) {
+		$pass = 1;
+		$alert .= "<li>" . $alertname . "</li>";
+	}
+	if ( empty($_REQUEST['license']) ) {
+		$pass = 1;
+		$alert .= "<li>" . $emptylicense . "</li>";
+	} elseif ( ereg( "[][{}()*+?.\\^$|]", $_REQUEST['license'] ) ) {
+		$pass = 1;
+		$alert .= "<li>" . $alertlicense . "</li>";
+	} elseif ($data_count == 0 || $data_count > 1) {
+		$pass = 1;
+		$alert .= "<li>" . $invalidlicense . " ...</li>";
+	} elseif ($user_email != 'Not Used' || $user_name != 'Not Used') {
+		$pass = 1;
+		$alert .= "<li>" . $invalidlicense . "  ...</li>";
+	}
+	if ( empty($_REQUEST['email']) ) {
+		$pass = 1;
+		$alert .= "<li>" . $emptyemail . "</li>";
+	} elseif ( !eregi("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$", $_REQUEST['email']) ) {
+		$pass = 1;
+		$alert .= "<li>" . $alertemail . "</li>";
+	}
+	
+	//If the user err'd, print the error messages.
+	if ( $pass==1 ) {
+
+		//This first line is for ajax/javascript, comment it or delete it if this isn't your cup o' tea.
+	echo "<script>$(\".message\").hide(\"slow\").show(\"slow\"); </script>";
+	echo "<b>" . $errormessage . "</b>";
+	echo "<ul>";
+	echo $alert;
+	echo "</ul>";
+	 
+
+	// If the user didn't err and there is in fact a message, time to email it.
+	} elseif ($pass==0) {
+		$time = current_time('mysql');
+		$email = $_REQUEST['email'];
+		$name = $_REQUEST['name'];
+		$p_code = $partner_code;
+		$wpdb->update( $table_name, array('time_entered' => $time, 'user_email' => $email, 'user_name' => $name), array( 'partner_code' => $p_code ), array( '%s', '%s', '%s'), array( '%s' ) );
+		//Construct the message.
+		$message = "\n\nA gift code has been submitted at ".$the_referer.".\n\n";
+	    $message .= "From: " . clean_var($_REQUEST['name']) . "\n";
+		$message .= "Email: " . clean_var($_REQUEST['email']) . "\n\n\n";
+		$message .= "Logitech Promotion Code: " . clean_var($_REQUEST['license']) . "\n\n\n";
+		
+		$message .= "Go to " . $redURL . "\n";
+	    $message .= "and enter the code, ".$partner_code. " to complete the process and ". $end_message;
+		
+	    $header = 'From:'. $sendFrom;
+		$headers .= 'Bcc: '. $bcc . "\r\n";
+	    if ($ptype == 'Connect') {
+			$conURL = $_REQUEST['conURL'] ;
+			$conNAME = $_REQUEST['conNAME'] ;
+			$conTNAME = $_REQUEST['conTNAME'] ;
+			$conUSER = $_REQUEST['conUSER'] ;
+			$conPASSWORD = $_REQUEST['conPASSWORD'] ;
+			$conQSTRING = $_REQUEST['conQSTRING'] ;
+		} else if ($ptype == 'Query') {
+			$queV1 = $_REQUEST['queV1'] ;
+			$queV2 = $_REQUEST['queV2'] ;
+			$queV3 = $_REQUEST['queV3'] ;
+			// construct query string
+			$username = str_replace(" ","_",$name);
+			$theQUERY = $redURL.'?'.$queV1.'='.$username.'&'.$queV2.'='.$email.'&'.$queV3.'='.$partner_code;
+		}
+//Mail the message - for production
+
+		mail($sendto, $subject, $message, $header);
+//This is for javascript, 
+	    $instructions = clean_var($_REQUEST['name']) . ", thank you for entering your gift code.\n";
+		$instructions .= "The information needed to complete the process is listed below and will be emailed to, " . clean_var($_REQUEST['email']) . "<br><br>";
+		if ($ptype == 'Query') {
+			$instructions .= "Go to <a href='{$theQUERY}' target='_blank'>" . $redURL . "</a>, ";
+		} else {
+			$instructions .= "Go to <a href='{$redURL}' target='_blank'>" . $redURL . "</a>, ";
+		}
+	    $instructions .= " and enter the code: ".$partner_code .' '. $end_message;
+		echo "<script>$(\".message\").hide(\"slow\").show(\"slow\").animate({opacity: 1.0}, 4000); $(':input').clearForm() </script>";
+		echo $instructions."<br>";
+		//echo $thanks;
+		if ($ptype == 'Query') {
+			echo '<script type="text/javascript" language="javascript"> 
+			window.open("'.$theQUERY.'"); 
+			</script>';
+		}
+		die();
+
+//Echo the email message - for development
+		//echo "<br/><br/>" . $message;
+
+	}
+	
+//If honeypot is filled, trigger the message that bot likely won't see.
+} else {
+	echo "<script>$(\".message\").hide(\"slow\").show(\"slow\"); </script>";
+	echo $honeypot;
+}
+
+?>
